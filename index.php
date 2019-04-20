4d1012cc53d9fc1a868d65fedf16ad5be74824a5
Renato Mendes <renatoccve@gmail.com>
2019-04-20 17:27:07 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE HTML>
+<html>
+	<html lang="pt-br">
+	<head>
+	
+	<?php include 'analyticstracking.php'; ?>		
+		<!--<script async src="https://cdn.ampproject.org/v0.js"></script>-->
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="canonical" href="www.renatomendes.com" />
+		<title>Quer ajuda? Projetos Digitais, Growth Hacking e Marketing Digital</title>
+		<meta name="description" content="Gestão de Projetos web, Growth Hacking, Marketing Digital, Inbound Marketing e Projetos Digitais. Zap (31) 99243 9412 - Quero te conhecer!">
+		<link rel="stylesheet" href="assets/css/compactado_main.css" />
+
+	<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/compactado_util.js"></script>
+			<script src="assets/js/compactado_main.js"></script>
+
+	</head>
+
+	<body>
+
+	<!-- Wrapper -->
+		<div id="wrapper">
+
+		<!-- Header -->
+			<?php 	include 'header.html'; ?>
+		<!-- Header -->
+
+		<!-- Main -->
+			
+			<div id="main">
+
+				<?php 
+
+  						 include 'paginas/sobre.php'; 
+						 include 'paginas/portfolio.php'; 
+						 include 'paginas/expertises.php'; 
+						 include 'paginas/curriculo.php'; 
+						 include 'paginas/fale-comigo.php';
+						 include 'paginas/portfolio_desenvolvedor.php';
+						 include 'paginas/elements.php';  
+						 include 'paginas/resposta-formulario.php'; 
+
+					?>	
+			</div>
+		<!-- Main -->
+
+		<!-- Footer -->
+			<footer id="footer">
+				<p class="copyright"><a href="tel:31992439412"><span class="obrigado upper">( 031 ) 99243-9412</span></a> &copy; RenatoMendes.com</p>
+			</footer>
+		<!-- Footer -->
+
+	<!-- Wrapper -->
+		</div>
+
+<!-- BG-Internas -->
+	<div id="bg">
+		<!-- Conteúdo páginas Internas -->
+	</div>
+
+	</body>
+</html>
